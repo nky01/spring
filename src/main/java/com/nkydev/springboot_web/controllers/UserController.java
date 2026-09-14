b0a2adc93cee382a758dc72907ae2f640eaabac9
Nicole Cayo <nickybelu@gmail.com>
2026-09-13 22:35:18 -0300

    spring MVC: pass data to the view

--- src/main/java/com/nkydev/springboot_web/controllers/UserController.java
+++ src/main/java/com/nkydev/springboot_web/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.nkydev.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller 
 public class UserController {
 
     @GetMapping("/details")  
-    public String details(){
-        
+    public String details(Model model){
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name", "Nicole");
+        model.addAttribute("lastname", "Cayo");
         return "details";
     }
 }

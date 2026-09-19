dceb09335c3532f3ed9f25a346aeecb90d76e464
Nicole Cayo <nickybelu@gmail.com>
2026-09-18 23:36:38 -0300

    add object Model

--- src/main/java/com/nkydev/springboot_web/controllers/UserController.java
+++ src/main/java/com/nkydev/springboot_web/controllers/UserController.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.nkydev.springboot_web.models.User;;
+
 @Controller 
 public class UserController {
 
     @GetMapping("/details")  
     public String details(Model model){
+
+        User user = new User("Nicole", "Cayo");
+
         model.addAttribute("title", "Hola Mundo Spring Boot");
-        model.addAttribute("name", "Nicole");
-        model.addAttribute("lastname", "Cayo");
+        model.addAttribute("user", user);
         return "details";
     }
 }
--- src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nkydev.springboot_web.models.User;;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
@@ -14,11 +16,13 @@ public class UserRestController {
     @GetMapping(path = "/details")
     public Map<String, Object> details(){
 
+        User user = new User("Nicole", "Cayo");
+
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola Mundo Spring Boot");
-        body.put("name", "Nicole");
-        body.put("lastname", "Cayo");
+        body.put("user", user);
+
         return body;
     }
 }

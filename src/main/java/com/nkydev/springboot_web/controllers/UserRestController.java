396900f4f66602f2ee3593856f0450ea5e273e96
Nicole Cayo <nickybelu@gmail.com>
2026-09-18 23:21:16 -0300

    add @RequestMapping

--- src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
@@ -4,13 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @GetMapping(path = "/details")
     public Map<String, Object> details(){
+
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola Mundo Spring Boot");

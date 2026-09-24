bee984b3bb3a4c3e5810872669ed2f1e5e063fd1
Nicole Cayo <nickybelu@gmail.com>
2026-09-23 22:26:40 -0300

    implement DTO and create userDTO package

--- src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/nkydev/springboot_web/controllers/UserRestController.java
@@ -1,28 +1,25 @@
 package com.nkydev.springboot_web.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nkydev.springboot_web.models.User;;
+import com.nkydev.springboot_web.models.User;
+import com.nkydev.springboot_web.models.dto.UserDTO;;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping(path = "/details")
-    public Map<String, Object> details(){
+    public UserDTO details(){
 
         User user = new User("Nicole", "Cayo");
 
-        Map<String, Object> body = new HashMap<>();
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUser(user);
+        userDTO.setTitle("Hola Mundo Spring Boot");
 
-        body.put("title", "Hola Mundo Spring Boot");
-        body.put("user", user);
-
-        return body;
+        return userDTO;
     }
 }
---
+++ src/main/java/com/nkydev/springboot_web/models/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.nkydev.springboot_web.models.dto;
+
+import com.nkydev.springboot_web.models.User;
+
+public class UserDTO {
+    
+    private String title;
+    private User user;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }    
+}
